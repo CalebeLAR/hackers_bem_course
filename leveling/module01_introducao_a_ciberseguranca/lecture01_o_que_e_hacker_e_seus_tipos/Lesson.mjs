09425e7515669b816172ec67da2d37d4e4edec4a
calebeLAR <ca6ele@gmail.com>
2024-02-02 17:18:48 -0300

    lecture01_o_que_e_hacker_e_seus_tipos && git commit -m lecture02_conceito_de_etica

---
+++ leveling/module01_introducao_a_ciberseguranca/lecture01_o_que_e_hacker_e_seus_tipos/Lesson.mjs
@@ -0,0 +1,9 @@
+export default class Lesson {
+  static topics = [
+    'Para começar',
+    'Aula 1 - O que é hacker e seus tipos',
+    'Conceito e tipos de hacker',
+    'Quais são os tipos de hackers?',
+  ];
+  static practicing = ['100% 3 de tres acertos'];
+}
---
+++ leveling/module01_introducao_a_ciberseguranca/lecture02_conceito_de_etica/Lesson.mjs
@@ -0,0 +1,11 @@
+export default class Lesson {
+  static topics = [
+    'Aula 2 - Conceito de ética',
+    'Introdução à ética',
+    'Hackers éticos',
+    'Como se tornar um hacker ético?',
+    'Atividades de um hacker ético',
+    'O que faz?',
+  ];
+  static practicing = ['100%, 3 de 3 corretas'];
+}
