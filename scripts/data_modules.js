86f3c6bb06b732aa53437d34c2aea7c12c9240aa
Ca3ele <ca6ele@gmail.com>
2024-03-08 18:20:43 -0300

    scripts

--- scripts/data_modules.js
+++ scripts/data_modules.js
@@ -1,13 +1,13 @@
 const { formatTitle } = require('./helpers');
 
 const dataLetures = [
-  'Aula 1 - Entrada, Saída e Processamento',
-  'Aula 2 - Variável',
-  'Aula 3 - Estrutura Condicional',
-  'Aula 4 - Estrutura de Repetição',
-  'Recapitulando',
+  "Aula 1 - Scripts Windows",
+  "Aula 2 - Scripts Linux",
+  "Recapitulando,",
 ];
 
-const dataModule = formatTitle('Module09 - Lógica de Programação');
+const dataModule = formatTitle('Module10 - Desenvolvimento de Scripts');
 
 module.exports = { dataLetures, dataModule };
+
+
